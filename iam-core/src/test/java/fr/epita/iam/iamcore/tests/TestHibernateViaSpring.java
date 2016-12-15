216343201763c0f0925fd8c6950f10d6f0dcf89e
tbrou <tbrou@tbrou-PC7.mshome.net>
2016-12-15 19:17:47 +0100

    * add : iam-parent

--- iam-core/src/test/java/fr/epita/iam/iamcore/tests/TestHibernateViaSpring.java
+++ iam-core/src/test/java/fr/epita/iam/iamcore/tests/TestHibernateViaSpring.java
@@ -5,8 +5,13 @@ package fr.epita.iam.iamcore.tests;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.sql.DataSource;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import fr.epita.iamcore.datamodel.Identity;
 import fr.epita.iamcore.services.dao.IdentityDAO;
@@ -15,13 +20,25 @@ import fr.epita.iamcore.services.dao.IdentityDAO;
  * @author tbrou
  *
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"/applicationContext.xml"})
 public class TestHibernateViaSpring {
+	
+	@Inject
+	DataSource ds;
 	
 	@Inject
 	@Named("identityDAO")
 	private IdentityDAO dao;
 	
+	
 	@Test
+	public void testDataSource(){
+		Assert.assertNotNull(ds);
+	}
+	
+	//@Test
 	public void testConfiguration(){
 		Identity identity = new Identity();
 		identity.setDisplayName("Thomas");
